facf1a10a62613b77bad0ed76500c7abbeb13aab
Belindaanindya <belindaanindya@gmail.com>
2020-10-13 15:20:44 +0700

    Database Seeder

--- database/seeders/TagTableSeeder.php
+++ database/seeders/TagTableSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
 
 class TagTableSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class TagTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create('id_ID');
+        for($i = 1; $i <= 20; $i++){
+            // insert data ke table user
+            DB::table('tag_table')->insert([
+                'user_nama' => $faker->word,
+            ]);
+        }  
     }
 }
