b9e033af462e4a9627ef60591d47468d5e4e7ba2
zulfauzi92 <64009520+zulfauzi92@users.noreply.github.com>
2020-10-18 21:34:55 +0700

    Update RatingReviews.php

--- app/Models/RatingReviews.php
+++ app/Models/RatingReviews.php
@@ -9,26 +9,6 @@ class RatingReviews extends Model
 {
     use HasFactory;
 
-<<<<<<< HEAD:app/Models/CategoryLocation.php
-    
-    protected $table = 'rating_reviews';
-    public $timestamps = true;
-
-    protected $fillable = [
-<<<<<<< Updated upstream:app/Models/CategoryLocation.php
-        'name',
-        'address',
-        'description',
-        'image',
-        'contact',
-        'latitude',
-        'longitude',
-=======
-        'rating',
-        'review',
-        'created_at',
->>>>>>> Stashed changes:app/Models/RatingReviews.php
-=======
     protected $table = "rating_reviews";
     public $timestamps = true;
 
@@ -36,15 +16,16 @@ class RatingReviews extends Model
         'created_at',
         'rating',
         'review',
->>>>>>> 23a91847dfb9171e60e2be688a004f415d57b56d:app/Models/RatingReviews.php
+        
     ];
 
-    
+   
     protected $hidden = [
     ];
 
-    
+   
     protected $casts = [
-        
+        'latitude' => 'double',
+        'longitude' => 'double',
     ];
 }
